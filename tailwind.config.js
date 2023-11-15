c6899980c204b0ab19a4cb8bb4323c7789f3bdc3
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-11-15 15:36:29 +0100

    Remove unused imports from tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import { addDynamicIconSelectors } from "@iconify/tailwind";
-import typography from "@tailwindcss/typography";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: false,
-  content: [
-    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
-};
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+}

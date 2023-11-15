86e1a5c4d546f526cb03069b19e6c721bf76b101
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-11-15 17:20:43 +0100

    Add add user

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,12 @@
+import AddUserVue from '@/views/AddUser.vue'
+// import UserListVue from '@/views/UserList.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    // {path:"/", component: UserListVue},
+    {path:"/add-user", component: AddUserVue}
   ]
 })
 
---
+++ src/views/AddUser.vue
@@ -0,0 +1,31 @@
+<template>
+  <SplitLayout as="form" title="Add user" leftContainerClass="flex flex-col justify-between">
+    <template #default>
+      <div class="flex gap-4">
+        <UserInput label="First Name" name="firstName" />
+        <UserInput label="Last Name" name="lastName" />
+      </div>
+			<AppButton type="submit" variant="solid" class="w-fit">
+				Update details
+			</AppButton>
+    </template>
+    <template #aside>
+      <img
+        src="@/assets/avatar_default.svg"
+        width="128"
+        height="128"
+        class="mx-auto rounded-full border-2 border-slate-300"
+      />
+      <AppButton as="input" type="file" name="avatar" variant="outline" class="gap-2 w-full mt-20"
+        ><Icon icon="material-symbols:photo-camera-rounded" /><span>Change photo</span></AppButton
+      >
+    </template>
+  </SplitLayout>
+</template>
+
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue'
+import UserInput from '@/components/UserInput.vue'
+import { Icon } from '@iconify/vue'
+import SplitLayout from '@/layouts/SplitLayout.vue'
+</script>

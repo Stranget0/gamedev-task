1d8cfa0b7f04ee9e6d1a891cce0e01d1be8e69fe
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-11-17 14:35:03 +0100

    Add add user button

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,9 +1,13 @@
 <template>
   <DefaultLayout title="User list">
-    <UserInput name="nameFilter" variant="solid" labelClass="flex justify-between items-center"
-      ><span class="text-slate-500">Search</span
-      ><Icon icon="material-symbols:search" class="h-5 w-5" />
-    </UserInput>
+    <div class="max-w-sm mx-auto flex flex-col-reverse justify-center items-center gap-8 sm:gap-2 sm:max-w-none sm:justify-between sm:flex-row">
+			<UserInput name="nameFilter" variant="solid" containerClass="max-w-sm" labelClass="flex justify-between items-center"
+				><span class="text-slate-500">Search</span
+				><Icon icon="material-symbols:search" class="h-5 w-5" />
+			</UserInput>
+
+			<AppButton variant="solid-pill" class="ml-auto" :as="RouterLink" to="/add-user">Add user</AppButton>
+		</div>
 
     <AppList
       v-if="userResponse"
@@ -33,6 +37,8 @@ import { getUsers } from '@/api/userQueries'
 import UserListItem from '@/components/UserListItem.vue'
 import AppPagination from '@/components/AppPagination.vue'
 import { computed } from 'vue'
+import AppButton from '@/components/AppButton.vue'
+import { RouterLink } from 'vue-router'
 
 const userListLabels = [
   { text: 'Full name', class: 'col-start-2' },
